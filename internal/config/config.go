5ae4c4cebd1aec1debe4105b133d12ef85407629
Stephan Sedlmeier <stephan2048@gmail.com>
2020-08-31 12:14:33 +0200

    add support for config from environment

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"github.com/spf13/viper"
 	"log"
+	"strings"
 )
 
 type Config struct {
@@ -16,17 +17,28 @@ func Read() *Config {
 	cfg := &Config{}
 
 	v := viper.New()
+	replacer := strings.NewReplacer(".", "_")
+	v.SetEnvKeyReplacer(replacer)
+
 	v.SetConfigName("docsis-pnm")
 	v.AddConfigPath(".")
 	v.AddConfigPath("/etc/docsis-pnm")
 
-	v.SetDefault("snmp.community", "public")
-	v.SetDefault("snmp.retries", 3)
-	v.SetDefault("snmp.timeout", 5)
+	v.SetEnvPrefix("pnm")
+	v.AutomaticEnv()
 
+	// Don't forget to call SetDefault on every key, or the environment variables won't work
+	v.SetDefault("snmp.community", "public")
+	v.SetDefault("snmp.timeout", 5)
+	v.SetDefault("snmp.retries", 3)
 	v.SetDefault("snmp.workercount", 200)
 	v.SetDefault("snmp.modempollinterval", 900)
+
+	v.SetDefault("db.connstr", "")
 	v.SetDefault("db.commitinterval", 60)
+
+	// set defaults
+	v.SetDefault("tftp.externaladdress", "")
 
 	if err := v.ReadInConfig(); err != nil {
 		log.Fatalf("Error reading config file, %s", err)
@@ -38,4 +50,3 @@ func Read() *Config {
 
 	return cfg
 }
-
